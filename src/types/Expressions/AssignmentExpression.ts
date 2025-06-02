4f49b78a081a14265fa0944c2178b21834a15d8f
Keon Oh Kim <keonoh00@gmail.com>
2025-06-02 17:51:03 +0900

    Update AssignmentExpression

--- src/types/Expressions/AssignmentExpression.ts
+++ src/types/Expressions/AssignmentExpression.ts
@@ -3,4 +3,5 @@ import { ASTNodeTypes, IBaseNode } from "../BaseNode/BaseNode";
 export interface IAssignmentExpression extends IBaseNode {
   name: string;
   nodeType: ASTNodeTypes.AssignmentExpression;
+  operator: string;
 }
