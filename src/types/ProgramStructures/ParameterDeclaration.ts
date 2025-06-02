f6f372f16627938960a2b08c51164b2d6c503dac
Keon Oh Kim <keonoh00@gmail.com>
2025-06-02 12:50:09 +0900

    ParameterDeclaration Node Type

---
+++ src/types/ProgramStructures/ParameterDeclaration.ts
@@ -0,0 +1,8 @@
+import { ASTNodeTypes, IBaseNode } from "../BaseNode/BaseNode";
+
+export interface IParameterDeclaration extends IBaseNode {
+  name: string;
+  nodeType: ASTNodeTypes.ParameterDeclaration;
+  size: string;
+  type: string;
+}
