4bd148185b92c8764595e70820a601b1c9b063db
Keon Oh Kim <keonoh00@gmail.com>
2025-06-02 12:47:53 +0900

    Identifier Node Type

---
+++ src/types/Expressions/Identifier.ts
@@ -0,0 +1,8 @@
+import { ASTNodeTypes, IBaseNode } from "../BaseNode/BaseNode";
+
+export interface IIdentifier extends IBaseNode {
+  name: string;
+  nodeType: ASTNodeTypes.Identifier;
+  size: string;
+  type: string;
+}
