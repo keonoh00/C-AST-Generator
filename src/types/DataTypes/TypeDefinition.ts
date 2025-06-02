21d642c4493f20ad8b3fb92c5f15c30f23241d8a
Keon Oh Kim <keonoh00@gmail.com>
2025-06-02 12:46:37 +0900

    TypeDefinition Node Type

---
+++ src/types/DataTypes/TypeDefinition.ts
@@ -0,0 +1,7 @@
+import { ASTNodeTypes, IBaseNode } from "../BaseNode/BaseNode";
+
+export interface ITypeDefinition extends IBaseNode {
+  name: string;
+  nodeType: ASTNodeTypes.TypeDefinition;
+  underlyingType: unknown;
+}
