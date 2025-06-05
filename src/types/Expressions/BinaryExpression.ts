f5eb2af69d786d4600ac7e95104da3489f1a7d2d
Keon Oh Kim <keonoh00@gmail.com>
2025-06-05 09:43:12 +0900

    Add dropped type property in IBinaryExpression

--- src/types/Expressions/BinaryExpression.ts
+++ src/types/Expressions/BinaryExpression.ts
@@ -3,4 +3,5 @@ import { ASTNodeTypes, IBaseNode } from "../BaseNode/BaseNode";
 export interface IBinaryExpression extends IBaseNode {
   nodeType: ASTNodeTypes.BinaryExpression;
   operator: string;
+  type: string;
 }
