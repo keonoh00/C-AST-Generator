bf3a7c339d25e2d609c21330045c4dce2602ee68
Keon Oh Kim <keonoh00@gmail.com>
2025-06-05 09:31:18 +0900

    Fix IAssignmentExpression not using name property

--- src/types/Expressions/AssignmentExpression.ts
+++ src/types/Expressions/AssignmentExpression.ts
@@ -1,7 +1,6 @@
 import { ASTNodeTypes, IBaseNode } from "../BaseNode/BaseNode";
 
 export interface IAssignmentExpression extends IBaseNode {
-  name: string;
   nodeType: ASTNodeTypes.AssignmentExpression;
   operator: string;
 }
