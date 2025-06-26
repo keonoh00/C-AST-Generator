be4f33922805254468eeea826cbab940583921ac
Keon Oh Kim <keonoh00@gmail.com>
2025-06-26 14:14:46 +0900

    PostProcessor: Refactor removeInvlidNodes

--- src/joern/kast/PostProcessor.ts
+++ src/joern/kast/PostProcessor.ts
@@ -6,22 +6,22 @@ export class PostProcessor {
    * inlining their children instead.
    */
   public removeInvalidNodes(nodes: ASTNodes[]): ASTNodes[] {
-    const validateNode = (node: ASTNodes): ASTNodes[] => {
-      const nodeKeys = Object.keys(node);
-      if (!nodeKeys.includes("nodeType")) {
-        // Inline grandchildren
-        return (node.children ?? []).flatMap((child) => validateNode(child));
-      }
-      // Otherwise, keep this node but recurse into its children
-      const processedChildren = (node.children ?? []).flatMap((child) => validateNode(child));
-      return [
-        {
-          ...node,
-          children: processedChildren,
-        },
-      ];
-    };
+    return nodes.flatMap((node) => this.validateNode(node));
+  }
 
-    return nodes.flatMap((node) => validateNode(node));
+  private validateNode(node: ASTNodes): ASTNodes[] {
+    const nodeKeys = Object.keys(node);
+    if (!nodeKeys.includes("nodeType")) {
+      // Inline grandchildren
+      return (node.children ?? []).flatMap((child) => this.validateNode(child));
+    }
+    // Otherwise, keep this node but recurse into its children
+    const processedChildren = (node.children ?? []).flatMap((child) => this.validateNode(child));
+    return [
+      {
+        ...node,
+        children: processedChildren,
+      },
+    ];
   }
 }
